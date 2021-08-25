dce707600dff869946660e119156ae0efd559096
slawless08 <samuel.l.lawless@gmail.com>
2021-08-25 12:54:38 -0400

    Edited seeds to seed the database properly. Fixed some errors in routes. Code is finalized!

--- models/Category.js
+++ models/Category.js
@@ -8,9 +8,10 @@ Category.init(
   {
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncremenet: true,
-      allowNull: false
+      
     },
     category_name: {
       type: DataTypes.STRING,
