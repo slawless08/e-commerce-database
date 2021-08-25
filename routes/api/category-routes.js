e243eae19b27c08473daf22dedf063feeca55a3f
slawless08 <samuel.l.lawless@gmail.com>
2021-08-25 12:35:06 -0400

    Added CRUD routes for tag tables, and fixed error in category routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -71,6 +71,7 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id
       }
     });
+    res.status(200).json(categoryData);
   } catch(err) {
     res.status(500).json(err);
   }
